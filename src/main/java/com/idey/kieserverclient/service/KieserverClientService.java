c9be770b69f4410cb90ebe3c52cca8134e55c6a5
IndraniDey1 <IndraniDey@live.com>
2018-11-21 19:32:01 -0500

    Update KieserverClientService.java

--- src/main/java/com/idey/kieserverclient/service/KieserverClientService.java
+++ src/main/java/com/idey/kieserverclient/service/KieserverClientService.java
@@ -24,89 +24,6 @@ import com.idey.kieserverclient.domain.DeriveSeason;
 public class KieserverClientService {
 	
 	
-	public List<DeriveSeason> evaluateSeason(DeriveSeason deriveSeason)
-			throws IOException, ParseException, JSONException {
-
-		List<DeriveSeason> evaluatedDeriveSeasonList = new ArrayList();
-		
-		final String POST_PARAMS = "{\r\n" + "   \"lookup\":\"defaultStatelessKieSession\",\r\n"
-				+ "   \"commands\":[\r\n" + "      {\r\n" + "         \"insert\":{\r\n" + "            \"object\":{\r\n"
-				+ "               \"com.syngenta.dda.facts.DeriveSeason\":{\r\n"
-				+ "                  \"country\":\"" + deriveSeason.getCountry() + "\",\r\n" + "                  \"month\":\"" + deriveSeason.getMonth()+ "\",\r\n"
-				+ "                  \"harvestYear\":" + deriveSeason.getHarvestYear() + "\r\n" + "               }\r\n" + "            }\r\n"
-				+ "         }\r\n" + "      },     \r\n" + "      {\r\n" + "         \"fire-all-rules\":{\r\n"
-				+ "         	 \"max\" : -1,\r\n" + "         \"out-identifier\": \"output\"}\r\n" + "      },\r\n"
-				+ "      {\r\n" + "       \"get-objects\" : {\r\n" + "       \"out-identifier\" : \"output\" }\r\n"
-				+ "     }\r\n" + "    \r\n" + "   ]\r\n" + "}";
-		
-		
-		Authenticator.setDefault(new Authenticator() {
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("adminUser", "syngenta1!".toCharArray());
-			}
-		});
-		
-		URL obj = new URL(
-				"http://sadie-rules-kieserver-dda-sadie-rules.apps.usae-2.syngentaaws.org:80/services/rest/server/containers/instances/ag-reporting-rules-1");
-		HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
-		postConnection.setRequestMethod("POST");
-		postConnection.setRequestProperty("Content-Type", "application/json");
-		postConnection.setDoOutput(true);
-		OutputStream os = postConnection.getOutputStream();
-		os.write(POST_PARAMS.getBytes());
-		os.flush();
-		os.close();
-		int responseCode = postConnection.getResponseCode();
-		
-		if (responseCode == HttpURLConnection.HTTP_OK) { // success
-			BufferedReader in = new BufferedReader(new InputStreamReader(postConnection.getInputStream()));
-			String inputLine;
-			StringBuffer response = new StringBuffer();
-			while ((inputLine = in.readLine()) != null) {
-				response.append(inputLine);
-			}
-			in.close();
-			
-
-			System.out.println("'" + response.toString() + "'");
-			JSONObject jsonObj1 = new JSONObject(response.toString());
-			System.out.println(jsonObj1.get("result"));
-
-			JSONObject jsonObj2 = new JSONObject(jsonObj1.get("result").toString());
-			System.out.println(jsonObj2.get("execution-results"));
-
-			JSONObject jsonObj3 = new JSONObject(jsonObj2.get("execution-results").toString());
-			System.out.println(jsonObj3.get("results"));
-
-			JSONArray jArray = new JSONArray(jsonObj3.get("results").toString());
-			System.out.println("array length" + jArray.length());
-
-			for (int i = 0; i < jArray.length(); i++) {
-				JSONObject jb = new JSONObject(jArray.getJSONObject(i).toString());
-				
-				JSONArray jArray1 = new JSONArray(jb.get("value").toString());
-				
-				for (int j = 0; j < jArray.length(); j++) {
-					JSONObject deriveSeasonNode = new JSONObject(jArray1.getJSONObject(j).toString());
-
-					JSONObject deriveSeasonChildNode = new JSONObject(
-					deriveSeasonNode.get("com.syngenta.dda.facts.DeriveSeason").toString());
-
-					DeriveSeason ds = new DeriveSeason();
-					ds.setCountry(deriveSeasonChildNode.get("country").toString());
-					ds.setMonth(deriveSeasonChildNode.get("month").toString());
-					ds.setHarvestYear(new Integer(deriveSeasonChildNode.get("harvestYear").toString()));
-					// ds.setIsValidderiveSeasonJSon.get("country").toString()););
-					ds.setSeason(deriveSeasonChildNode.get("season").toString());
-
-					evaluatedDeriveSeasonList.add(ds);
-				}
-			}
-		} else {
-			System.out.println("POST NOT WORKED");
-		}
-		return evaluatedDeriveSeasonList;
-	}
 	
 	public List<DeriveSeason> evaluateSeasonList(List<DeriveSeason> deriveSeasonList)
 			throws IOException, ParseException, JSONException {
@@ -155,9 +72,8 @@ public class KieserverClientService {
 			}
 		});
 		
-		URL obj = new URL(
-				"http://sadie-rules-kieserver-dda-sadie-rules.apps.usae-2.syngentaaws.org:80/services/rest/server/containers/instances/ag-reporting-rules-1");
-		
+		URL obj = new URL("kieserver url here";
+				
 		HttpURLConnection postConnection = (HttpURLConnection) obj.openConnection();
 		postConnection.setRequestMethod("POST");
 		postConnection.setRequestProperty("Content-Type", "application/json");
@@ -209,7 +125,7 @@ public class KieserverClientService {
 				JSONObject deriveSeasonNode = new JSONObject(jArray1.getJSONObject(j).toString());
 
 				JSONObject deriveSeasonChildNode = new JSONObject(
-				deriveSeasonNode.get("com.syngenta.dda.facts.DeriveSeason").toString());
+				deriveSeasonNode.get("com.idey.DeriveSeason").toString());
 
 				
 				ds.setCountry(deriveSeasonChildNode.get("country").toString());
